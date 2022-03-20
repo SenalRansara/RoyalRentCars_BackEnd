4a698973e6713ab82a77d0b0cb949733e8f58b4e
Senal Ransara <senal.ransara38@gmail.com>
2022-03-20 19:45:23 +0530

    configure the port and cretaed the DB connection

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require ("dotenv").config();
+
+const app = express();
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(cors())
+
+const PORT = 8000;
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL).then(()=> {
+    console.log('DB Connected Successfully');
+})
+.catch((err) => console.log('DB Connection Error',err));
+
+app.listen(PORT,()=>{
+    console.log(`App is running on ${PORT}`);
+});
